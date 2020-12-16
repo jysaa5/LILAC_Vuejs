9da8b72eb4dbe4ceadf33850f5cfa2a19a52de41
jysaa5 <jysaa5@naver.com>
2020-12-16 22:59:57 +0900

    update project

--- Infearn_Vue_1.0/vue-cli/src/App.vue
+++ Infearn_Vue_1.0/vue-cli/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ str }}
+    <!-- <app-header v-bind:하위 컴포넌트에서 정한 프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import를 사용할 때는 .vue까지 붙여서 사용해야 한다.
+import AppHeader from "./components/AppHeader.vue";
 export default {
-  name: 'App',
+  // 새 객체를 반환
+  data: function() {
+    return {
+      str: "Hi? Header",
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    "app-header": AppHeader,
+  },
+  methods: {
+    renewStr: function() {
+      this.str = "hi";
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
