c88e4b2abd50ed6320603ff4448a253fc4cc985d
jysaa5 <jysaa5@naver.com>
2021-02-09 00:13:14 +0900

    Study Vue.js

--- Infearn_Vue_1.2/vue-news/src/App.vue
+++ Infearn_Vue_1.2/vue-news/src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <div id="app">
-<router-view></router-view>
+    <!-- 1. Strongly Recommended -->
+    <!-- <ToolBar></ToolBar> -->
+    <!-- 2. Essential -->
+    <tool-bar></tool-bar>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
+export default {
+  components:{
+    ToolBar,
+  }
+}
 </script>
 
 <style>
