8339602eb45e52ee09242bf6189acb81baebf1fd
jysaa5 <jysaa5@naver.com>
2021-02-10 00:00:10 +0900

    Update NewsView.vue

--- Infearn_Vue_1.2/vue-news/src/views/NewsView.vue
+++ Infearn_Vue_1.2/vue-news/src/views/NewsView.vue
@@ -17,12 +17,16 @@ export default {
         // 바인딩 문제 때문.
         var vm = this;
         // axios.get('https://api.hnpwa.com/v0/news/1.json')
+        console.log('호출 전:', this);
         fetchNewsList()
-        .then(function(response){
+        // .then(function(response){
+            .then((response)=>{
             console.log(response);
             vm.users = response.data;
+            console.log('호출 후:', this)
         })
-        .catch(function(error){
+        // .catch(function(error){
+            .catch((error) => {
             console.log(error);
         })
     }
