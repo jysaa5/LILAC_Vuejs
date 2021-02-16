c658866b35c2f90c3b2378e8bb81a4342d879390
jysaa5 <jysaa5@naver.com>
2021-02-16 23:29:39 +0900

    Study Vue.js

--- Infearn_Vue_1.2/vue-news/src/views/ItemView.vue
+++ Infearn_Vue_1.2/vue-news/src/views/ItemView.vue
@@ -2,21 +2,25 @@
     <div>
         <section>
             <!-- 질문 상세 정보 -->
+            <div class="user-container">
             <div>
-                User
+                <i class="fas fa-user"></i>
             </div>
-            <div>
+            <div class="user-description">
                 <router-link :to="`/user/${fetchedItem.user}`">
                 {{ fetchedItem.user }}
                 </router-link>
-                <div>
+                <div class="time">
                     {{fetchedItem.time_ago}}
                 </div>
+            </div>
             </div>
             <h2>{{fetchedItem.title}}</h2>
         </section>
         <section>
             <!-- 질문 댓글 -->
+            <div v-html="fetchedItem.content">
+            </div>
         </section>
         <!-- {{this.$store.state.item}} -->
         <!-- <p>{{this.$store.state.item.title}}</p>
@@ -36,5 +40,19 @@ export default {
     }
 }
 </script>
-<style>
+<style scoped>
+.user-container{
+    display: flex;
+    align-content: center;
+    padding: 0.5rem;
+}
+.fa-user{
+    font-size: 2.5rem;
+}
+.user-description{
+    padding-left: 8px;
+}
+.time{
+    font-size: 0.7rem;
+}
 </style>
