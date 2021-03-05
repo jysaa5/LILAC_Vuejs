47962a962170cb91b25592e104d9352bf45d0778
jysaa5 <jysaa5@naver.com>
2021-03-05 23:02:31 +0900

    Update index.js

--- Infearn_Vue_1.2/vue-news/src/routes/index.js
+++ Infearn_Vue_1.2/vue-news/src/routes/index.js
@@ -6,6 +6,10 @@ import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
 // import createListView from '../views/CreateListView.js';
+import bus from "../utils/bus.js";
+import {
+    store
+} from '../store/index.js';
 
 Vue.use(VueRouter);
 
@@ -23,6 +27,37 @@ export const router = new VueRouter({
             // component: NewsView,
             //component: createListView('NewsView'),
             component: NewsView,
+            beforeEnter: (to, from, next) => {
+                // console.log('to', to);
+                // console.log('from', from);
+                // console.log('next', next);
+                // next()
+
+                // 라우터로 넘어가기 위한 조건 추가
+
+                // # 1
+                // if (to.matched === '') {
+                //     next();
+                // }
+
+                // # 2
+                // if (to.auth) {
+                //     next();
+                // } else {
+                //     router.push('')
+                // }
+
+                bus.$emit('start:spinner');
+                store.dispatch('FETCH_LIST', to.name)
+                    .then(() => {
+                        console.log('fetched');
+                        bus.$emit('end:spinner');
+                        next();
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    })
+            }
         },
         {
             path: '/asks',
