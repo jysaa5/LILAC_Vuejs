1cb6246dfbe86c38e216450bb3ebef0055cbb9c3
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2021-09-14 00:00:27 +0900

    Update App.vue

--- To_do_list/to-do-list/src/App.vue
+++ To_do_list/to-do-list/src/App.vue
@@ -9,7 +9,7 @@
 
 <script>
 import TodoHeader from "./components/TodoHeader.vue";
-import TodoInput from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
 import TodoFooter from "./components/TodoFooter.vue";
 export default {
