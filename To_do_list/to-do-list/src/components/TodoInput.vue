df8969d8cae3de2a0046612ae7cd0ddfaa3e5114
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2021-09-15 01:35:03 +0900

    Update TodoInput.vue

--- To_do_list/to-do-list/src/components/TodoInput.vue
+++ To_do_list/to-do-list/src/components/TodoInput.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>input</div>
+  <div>
+    <input type="text" v-model="newTodoItem" />
+    <button v-on:click="addTodo">추가</button>
+  </div>
 </template>
 <script>
 // 할 일 입력 및 추가
-export default {};
+export default {
+  data() {
+    return {
+      newTodoItem: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      console.log(this.newTodoItem);
+    },
+  },
+};
 </script>
