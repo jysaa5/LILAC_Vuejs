e7eaf6e518604652eedb9fd95e87c9db2cdbb7a0
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2021-09-19 22:02:45 +0900

    Update TodoList.vue

--- To_do_list/to-do-list/src/components/TodoList.vue
+++ To_do_list/to-do-list/src/components/TodoList.vue
@@ -1,8 +1,25 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems" :key="todoItem + '-' + i">{{ todoItem }}</li>
+    </ul>
+  </section>
 </template>
 <script>
 // 할 일 목록 표시 및 특정 할 일 삭제
-export default {};
+export default {
+  data() {
+    return {
+      todoItems: [],
+    };
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+};
 </script>
 <style></style>
