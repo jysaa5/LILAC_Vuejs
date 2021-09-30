41f81d1a2555f7d689b9c85e27340414c87ca278
jysaa5 <57422057+jysaa5@users.noreply.github.com>
2021-09-30 20:30:44 +0900

    update project

--- To_do_list/to-do-list/src/components/TodoInput.vue
+++ To_do_list/to-do-list/src/components/TodoInput.vue
@@ -4,14 +4,27 @@
     <span class="addContainer" v-on:click="addTodo">
       <i class="addBtn fas fa-plus" aria-hidden="true"></i>
     </span>
+    <modal v-if="showModal" @close="showModal = false">
+      <h3 slot="header">경고</h3>
+      <span slot="footer" @click="showModal = false">
+        할 일을 입력하세요.
+        <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+      </span>
+    </modal>
   </div>
 </template>
 <script>
+import Modal from "./common/Modal.vue";
 // 할 일 입력 및 추가
 export default {
+  components: {
+    Modal: Modal,
+  },
+  props: ["propsdata"],
   data() {
     return {
       newTodoItem: "",
+      showModal: false,
     };
   },
   methods: {
@@ -24,6 +37,8 @@ export default {
         // localStorage.setItem(value, value);
         this.$emit("addTodo", value);
         this.clearInput();
+      } else {
+        this.showModal = !this.showModal;
       }
     },
     clearInput() {
