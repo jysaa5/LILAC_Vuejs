80d5d0c45c0c64ef52dac11e35781beb48b31a36
jysaa5 <jysaa5@naver.com>
2022-04-12 01:49:47 +0900

    Update template and add div

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,3 +1,19 @@
 <template>
-  <div>JooYeon Kim</div>
+  <div class="name">{{ name }}</div>
+  <div>Hello World</div>
 </template>
+<script>
+export default {
+  setup() {
+    const name = "JooYeon Kim";
+    return {
+      name,
+    };
+  },
+};
+</script>
+<style scoped>
+.name {
+  color: red;
+}
+</style>
