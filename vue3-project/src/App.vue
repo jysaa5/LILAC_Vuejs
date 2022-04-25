d98e60d87ab61627fcbdb6931f4f6c408c5e0fd5
jysaa5 <jysaa5@naver.com>
2022-04-26 00:16:49 +0900

    v-bind에 input value 바인딩

--- vue3-project/src/App.vue
+++ vue3-project/src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <input v-bind:type="type" v-model="name" />
-  <button class="btn btn-primary" @click="onSubmit">Click</button>
+  <input v-bind:type="type" v-bind:value="name" />
+  <button class="btn btn-primary" @click="updateName">Click</button>
 </template>
 <script>
 import { ref } from "vue";
 export default {
   setup() {
     const name = ref("JooYeon");
-    const type = ref("text");
+    const type = ref("number");
 
     const onSubmit = () => {
       console.log(name.value);
     };
 
-    const updateName = (e) => {
-      console.log(e.target.value);
-      name.value = e.target.value;
+    const updateName = () => {
+      name.value = "Developer";
+      type.value = "text";
     };
 
     return {
